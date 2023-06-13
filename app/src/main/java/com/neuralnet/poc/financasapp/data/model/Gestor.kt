ff2d3ddb65bebcfcf0b2f9f775469ef55bde8cd5
Gustavo Almeida <gualmeida2004@gmail.com>
2023-06-13 15:59:40 -0300

    Utilizando a biblioteca kotlinx.serlialization

--- app/src/main/java/com/neuralnet/poc/financasapp/data/model/Gestor.kt
+++ app/src/main/java/com/neuralnet/poc/financasapp/data/model/Gestor.kt
@@ -1,5 +1,8 @@
 package com.neuralnet.poc.financasapp.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Gestor(
     val id: String,
     val nome: String,
--- app/src/main/java/com/neuralnet/poc/financasapp/di/AppModule.kt
+++ app/src/main/java/com/neuralnet/poc/financasapp/di/AppModule.kt
@@ -1,15 +1,15 @@
 package com.neuralnet.poc.financasapp.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.neuralnet.poc.financasapp.data.network.BASE_URL
 import com.neuralnet.poc.financasapp.data.network.PocFinancasApi
-import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -18,15 +18,12 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(moshi: Moshi): PocFinancasApi = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .build()
-        .create(PocFinancasApi::class.java)
+    fun provideFinancasApi(): PocFinancasApi = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(
+                Json.asConverterFactory("application/json".toMediaType())
+            )
+            .build()
+            .create(PocFinancasApi::class.java)
 
-    @Singleton
-    @Provides
-    fun provideMoshi(): Moshi = Moshi.Builder()
-        .addLast(KotlinJsonAdapterFactory())
-        .build()
 }
